493ececd75447a17ef1d9595c6652297c166a8bf
Piyushxz <140832863+Piyushxz@users.noreply.github.com>
2025-01-04 23:41:45 +0530

    added signup endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express"
+import { string, z } from "zod";
 const client = new PrismaClient()
 const app = express()
 
@@ -9,6 +10,50 @@ app.get("/user",async (req,res)=>{
     const users = await client.user.findMany()
 
     res.json({users})
+})
+
+
+app.get("signup",async (req,res)=>{
+
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    const age = req.body.age
+    const city = req.body.city
+
+   
+    const requiredBody = z.object({
+        username:z.string().min(50).max(5),
+        email:z.string().email(),
+        password:z.string().regex(/^[a-zA-Z0-9]+$/).min(5).max(50),
+        age:z.number().max(60).min(18),
+        city:z.string()
+
+    })
+    const parsedBody = requiredBody.safeParse(req.body)
+
+    if(!parsedBody.success){
+        res.status(400).json({error:"Invalid formart"})
+        return;
+    }
+
+    try{
+        await client.user.create({
+            data:
+            {
+                username:parsedBody.data?.username,
+                password:parsedBody.data?.password,
+                age:parsedBody.data?.age,
+                city:parsedBody.data?.city
+            }
+        }
+        )
+        res.status(200).json({message:"Created user succesfully"})
+    }catch(error){
+        res.status(500).json({message:"Could Not create user"})
+    }
+
+
 })
 
 
